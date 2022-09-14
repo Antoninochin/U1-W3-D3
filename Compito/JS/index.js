7377facd1884fec5e894921c543a43a59e5ffa01
Antoninochin <antonino_chindemi@virgilio.it>
2022-09-14 17:35:32 +0200

    aggiunto operatori

--- Compito/JS/index.js
+++ Compito/JS/index.js
@@ -17,14 +17,22 @@ function cambiaColore(colore){
 }
 console.log(cambiaColore) Scegliere in un solo pulsante*/ 
 
+let number1 = 1;
+let number2 = 2;
+console.log(2-1===1)
+console.log(number1 !== number2)
+console.log(number1 <= 2)
+console.log( number2 >= number1)
+
+
 function cambiaColore(color){
     document.getElementById('Premi').style.backgroundColor = color
 }
 
 let somma = sommaNumeri (2, 9, -4) 
-document.getElementById('risultato').innerHTML = somma;
 function sommaNumeri(n1, n2, n3) {
     return n1 + n2 + n3
 }
+document.getElementById('risultato').innerHTML = somma;
 document.write('somma')
 console.log(somma)
