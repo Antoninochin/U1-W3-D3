dbe3105169773f7fff8af56005787379044dca5f
Antoninochin <antonino_chindemi@virgilio.it>
2022-09-14 16:59:31 +0200

    compito

---
+++ Compito/JS/index.js
@@ -0,0 +1,30 @@
+//Cos'è una funzione
+//Come usare i parametri/argomenti
+//Keyword
+//Return
+
+/*function cambiaColore(colore){
+    document.getElementById("Premi").style.backgroundColor = colore;
+}
+function cambiaColore(colore){
+    document.getElementById("Premi").style.backgroundColor = colore;
+}
+function cambiaColore(colore){
+    document.getElementById("Premi").style.backgroundColor = colore;
+}
+function cambiaColore(colore){
+    document.getElementById("Premi").style.backgroundColor = colore;
+}
+console.log(cambiaColore) Scegliere in un solo pulsante*/ 
+
+function cambiaColore(color){
+    document.getElementById('Premi').style.backgroundColor = color
+}
+
+let somma = sommaNumeri (2, 9, -4) 
+document.getElementById('risultato').innerHTML = somma;
+function sommaNumeri(n1, n2, n3) {
+    return n1 + n2 + n3
+}
+document.write('somma')
+console.log(somma)
